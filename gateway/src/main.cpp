eaea8b15f9c9a13957eee6c73e860a3ed9e7c65c
Marco Recca <reccamarco@libero.it>
2024-02-12 17:18:37 +0100

    Update main.cpp

--- gateway/src/main.cpp
+++ gateway/src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <esp_now.h>
 #include <WiFi.h>
-#include <esp_now.h>
 #include <ArduinoBLE.h>
 #include <HTTPClient.h>
 #include <LoRa.h>
@@ -9,7 +8,7 @@
 
 
 //SSID of your network
-char ssid[] = "Readmi Note 12 5G";
+char ssid[] = "Redmi Note 12 5G";
 //password of your WPA Network
 char pass[] = "ciccio2305";
 
@@ -18,52 +17,18 @@ char pass[] = "ciccio2305";
 #define dio0 26
 
 
+bool SendToServer = false;
+String toSend = "";
+
 void onReceive(int packetSize) {
   if (packetSize == 0) return;          // if there's no packet, return
-
-  String incoming = "";                 // payload of packet
-
-  while (LoRa.available()) {            // can't use readString() in callback, so
-    incoming += (char)LoRa.read();      // add bytes one by one
-  }
-
-  Serial.println("Message: " + incoming);
-  HTTPClient http;
-    String url="http://192.168.60.129:5000/get_data";
-    String jsondata="{'datadatadatadatadatadatadatadatadatadatadatadatadatadatadata':'datadatadatadatadatadatadatadatadatadatadatadatadata'}";
-
-    http.begin(url); 
-    http.addHeader("Content-Type", "Content-Type: application/json"); 
-
-    int httpResponseCode = http.POST(jsondata); //Send the actual POST request
-
-    if(httpResponseCode>0){
-      String response = http.getString();  //Get the response to the request
-      Serial.println(httpResponseCode);   //Print return code
-      Serial.println(response);           //Print request answer
-    } else {
-      Serial.print("Error on sending POST: ");
-      Serial.println(httpResponseCode);
-
-      http.end();
-
-    }
-  
-  Serial.println("Messaggio ricevuto");
-  
-  
+  SendToServer = true;   
 }
 
 
 void setup() {
   // Init Serial Monitor
   Serial.begin(115200);
-
-  // Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_STA);
-  Serial.println();
-  WiFi.begin(ssid, pass);
-
 
   LoRa.setPins(ss, rst, dio0);
   while (!LoRa.begin(866E6)) {
@@ -76,10 +41,50 @@ void setup() {
   LoRa.receive();
   Serial.println("LoRa Initializing OK!");
 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  Serial.println();
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
   
 }
 
 
 void loop(){
+  if(SendToServer){
+    String incoming = "";                 // payload of packet
+
+    if (LoRa.available()) {            // can't use readString() in callback, so
+      incoming += LoRa.readString();
+      Serial.println("Message: " + incoming);
+      toSend = incoming;     
+    } 
+    
+    HTTPClient http;
+    String url="http://192.168.60.129:5000/get_data";
+    http.begin(url); 
+    http.addHeader("Content-Type", "Content-Type: application/json"); 
+
+    int httpResponseCode = http.POST(toSend); //Send the actual POST request
+
+    if(httpResponseCode>0){
+      String response = http.getString();  //Get the response to the request
+      Serial.println(httpResponseCode);   //Print return code
+      Serial.println(response);        //Print request answer
+      http.end();          
+    } else {
+      Serial.print("Error on sending POST: ");
+      Serial.println(httpResponseCode);
+
+      http.end();
+
+    }
   
+    Serial.println("Messaggio ricevuto");
+    SendToServer = false;
+  }
+  delay(100);
 }
