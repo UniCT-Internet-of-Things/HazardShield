6e87f8d0211bc3eff1b1eb49454bf9c3bf8d2c37
ciccio2305 <ciccio.conti0909@gmail.com>
2024-02-20 15:06:48 +0100

    comunicazione bidirezionale

--- back end/main.py
+++ back end/main.py
@@ -2,13 +2,28 @@ from flask import Flask, render_template, url_for, request, redirect, send_from_
 import pymongo as PyMongo
 import json
 import time
-
+import requests
+import threading
 frontend_folder = './build/'
 app = Flask(__name__,
             static_folder=frontend_folder+'_app',
             template_folder=frontend_folder,
             )
+global gateway_ip
+gateway_ip=""
 
+#create a thread
+
+def send_request():
+    while True:
+        #send request to the gateway
+        if gateway_ip != "":    
+            print("Sending request to the gateway")
+            response = requests.post("http://"+gateway_ip+":80/receive_data", data = {'datas': 'data', 'values': {"test":"2","test2":"4"}})   
+            print(response)
+            time.sleep(5)
+
+thread = threading.Thread(target=send_request)
 
 @app.route('/get_data', methods=['POST'])
 def ricevi_dati():
@@ -18,6 +33,26 @@ def ricevi_dati():
     print(request.data)
     return 'Dati ricevuti con successo!'
 
+@app.route('/register_ip', methods=['POST'])
+def register_ip():
+
+    print(request)
+    #stampa tutta la struttura della richiesta
+    print(request.data)
+
+    #convert data from byte to string
+    data = request.data.decode('utf-8')
+    print(data)
+    #convert data from string to dictionary
+    data = json.loads(data)
+    print(data)
+    #get the ip of the sender
+    global gateway_ip
+    gateway_ip = request.remote_addr
+    print(gateway_ip)
+    return 'Dati ricevuti con successo!'
 
 if __name__ == '__main__':
+    thread.start()
     app.run(debug=True,host="0.0.0.0")
+    
--- gateway/src/main.cpp
+++ gateway/src/main.cpp
@@ -4,7 +4,7 @@
 #include <ArduinoBLE.h>
 #include <HTTPClient.h>
 #include <LoRa.h>
-
+#include "esp_http_server.h"
 
 
 //SSID of your network
@@ -16,14 +16,72 @@ char pass[] = "ciccio2305";
 #define rst 23
 #define dio0 26
 
-
 bool SendToServer = false;
 String toSend = "";
 
+String base_url="http://192.168.231.130:5000/";
+
 void onReceive(int packetSize) {
-  Serial.println("Received a packet");
   if (packetSize == 0) return;          // if there's no packet, return
   SendToServer = true;   
+}
+
+esp_err_t post_handler(httpd_req_t *req)
+{
+    /* Destination buffer for content of HTTP POST request.
+     * httpd_req_recv() accepts char* only, but content could
+     * as well be any binary data (needs type casting).
+     * In case of string data, null termination will be absent, and
+     * content length would give length of string */
+    
+    char content[req->content_len+1];
+    content[req->content_len] = '\0';
+    int ret = httpd_req_recv(req, content, req->content_len);
+    if (ret <= 0) { 
+        if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
+            
+            httpd_resp_send_408(req);
+        }
+        return ESP_FAIL;
+    }
+    Serial.println(content);
+
+    /* Send a simple response */
+    const char resp[] = "URI POST Response";
+    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+/* URI handler structure for GET /uri */
+httpd_uri_t uri_post = {
+    .uri      = "/receive_data",
+    .method   = HTTP_POST,
+    .handler  = post_handler,
+    .user_ctx = NULL
+};
+
+httpd_handle_t Server;
+
+httpd_handle_t start_webserver(void)
+{
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+    /* Empty handle to esp_http_server */
+    httpd_handle_t server = NULL;
+
+    /* Start the httpd server */
+    if (httpd_start(&server, &config) == ESP_OK) {
+        /* Register URI handlers */
+        httpd_register_uri_handler(server, &uri_post);
+    }
+    /* If server failed to start, handle will be NULL */
+    return server;
+}
+
+void stop_webserver(httpd_handle_t server)
+{
+    /* Stop the httpd server */
+    httpd_stop(server);
 }
 
 
@@ -50,13 +108,41 @@ void setup() {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
-  
-}
 
+  Server= start_webserver();
+  Serial.println("created server");
+
+  delay(3000);
+
+  HTTPClient http;
+  String url=base_url+"register_ip";
+  http.begin(url);
+  http.addHeader("Content-Type", "Content-Type: application/json"); 
+
+  int httpResponseCode = http.POST(String("{\"my_ip\":\"")+String(WiFi.localIP())+String("\"}"));
+
+  if(httpResponseCode>0){
+    String response = http.getString();  //Get the response to the request
+    Serial.println(httpResponseCode);   //Print return code
+    Serial.println(response);        //Print request answer
+    http.end();          
+  } else {
+    Serial.print("Error on sending POST: ");
+    Serial.println(httpResponseCode);
+
+    http.end();
+
+  }
+
+  Serial.println("Messaggio ricevuto");
+  SendToServer = false;
+
+
+}
 
 void loop(){
   if(SendToServer){
-    String incoming = "";                 // payload of packet
+    String incoming = ""; 
 
     if (LoRa.available()) {            // can't use readString() in callback, so
       incoming += LoRa.readString();
@@ -65,7 +151,7 @@ void loop(){
     } 
     
     HTTPClient http;
-    String url="http://192.168.231.130:5000/get_data";
+    String url=base_url+"get_data";
     http.begin(url); 
     http.addHeader("Content-Type", "Content-Type: application/json"); 
 
